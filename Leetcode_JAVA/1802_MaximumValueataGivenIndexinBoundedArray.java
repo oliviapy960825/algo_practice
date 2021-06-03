b5e52300fa7629ef8cfbc8e0e7a260b54ad77252
Wang Peiyu <oliviapy@udel.edu>
2021-06-02 20:51:55 -0700

    added 1802

---
+++ Leetcode_JAVA/1802_MaximumValueataGivenIndexinBoundedArray.java
@@ -0,0 +1,62 @@
+/*
+Medium
+
+You are given three positive integers: n, index, and maxSum. You want to construct an array nums (0-indexed) that satisfies the following conditions:
+
+    nums.length == n
+    nums[i] is a positive integer where 0 <= i < n.
+    abs(nums[i] - nums[i+1]) <= 1 where 0 <= i < n-1.
+    The sum of all the elements of nums does not exceed maxSum.
+    nums[index] is maximized.
+
+Return nums[index] of the constructed array.
+
+Note that abs(x) equals x if x >= 0, and -x otherwise.
+
+ 
+
+Example 1:
+
+Input: n = 4, index = 2,  maxSum = 6
+Output: 2
+Explanation: nums = [1,2,2,1] is one array that satisfies all the conditions.
+There are no arrays that satisfy all the conditions and have nums[2] == 3, so 2 is the maximum nums[2].
+
+Example 2:
+
+Input: n = 6, index = 1,  maxSum = 10
+Output: 3
+
+ 
+
+Constraints:
+
+    1 <= n <= maxSum <= 109
+    0 <= index < n
+
+
+*/
+
+
+class Solution {
+    public long sum(int n, int index, int a){
+        int b = Math.max(a - index, 0);
+        long res = (long)(a + b) * (a - b + 1) / 2;
+        b = Math.max(a - ((n - 1) - index), 0);
+        res += (long)(a + b) * (a - b + 1) / 2;
+        return res - a;
+    }
+    
+    public int maxValue(int n, int index, int maxSum) {
+        maxSum -= n;
+        int left = 0, right = maxSum, mid;
+        while (left < right) {
+            mid = (left + right + 1) / 2;
+            if (sum(n, index, mid) <= maxSum)
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        return left + 1;
+    }
+}
