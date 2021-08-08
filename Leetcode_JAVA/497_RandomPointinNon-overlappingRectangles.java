12cd9568625e53486ad5f5ffd0ebdbed4ac91ddb
Wang Peiyu <oliviapy@udel.edu>
2021-08-08 14:27:47 -0700

    added 497

---
+++ Leetcode_JAVA/497_RandomPointinNon-overlappingRectangles.java
@@ -0,0 +1,42 @@
+/*
+Medium
+*/
+
+class Solution {
+    Random random;
+    TreeMap<Integer, int[]> map;
+    int areaSum = 0;
+    public Solution(int[][] rects) {
+        this.random = new Random();
+        this.map = new TreeMap();
+        for(int i = 0; i < rects.length; i++){
+            int [] rectangeCoordinates = rects[i];
+            int length = rectangeCoordinates[2] - rectangeCoordinates[0] + 1 ; // +1 as we need to consider edges also.
+            int breadth = rectangeCoordinates[3] - rectangeCoordinates[1] + 1 ;
+            
+            areaSum += length * breadth;
+            
+            map.put(areaSum,rectangeCoordinates);
+            
+        }
+    }
+    
+    public int[] pick() {
+        int key = map.ceilingKey(random.nextInt(areaSum) + 1);
+        int [] rectangle = map.get(key);
+        
+        int length = rectangle[2] - rectangle[0] + 1 ; // +1 as we need to consider edges also.
+        int breadth = rectangle[3] - rectangle[1] + 1 ;
+        
+        int x = rectangle[0] + random.nextInt(length); //return random length from starting position of x
+        int y = rectangle[1] + random.nextInt(breadth); // return random breadth from starting position of y
+        
+        return new int[]{x,y};
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(rects);
+ * int[] param_1 = obj.pick();
+ */
