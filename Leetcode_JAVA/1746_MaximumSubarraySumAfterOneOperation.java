4116dc24e3ff8bf25c8d557bfce846d4619f9ac1
Wang Peiyu <oliviapy@udel.edu>
2021-08-14 17:29:37 -0700

    added 1746

---
+++ Leetcode_JAVA/1746_MaximumSubarraySumAfterOneOperation.java
@@ -0,0 +1,21 @@
+/*
+Medium
+*/
+
+class Solution {
+    public int maxSumAfterOperation(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0] * nums[0];
+        int max = Math.max(dp[0][0], dp[0][1]);
+        for(int i = 1; i < n; i++){
+            dp[i][0] = Math.max(dp[i - 1][0] + nums[i], nums[i]);
+            dp[i][1] = Math.max(nums[i] * nums[i], Math.max(dp[i - 1][1] + nums[i], dp[i - 1][0] + nums[i] * nums[i]));
+            max = Math.max(max, dp[i][1]);
+        }
+        
+        return max;
+    }
+}
