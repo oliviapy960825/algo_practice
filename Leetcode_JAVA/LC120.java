acea0c04f890604eb380c6203c29fdde08a4b446
Wang Peiyu <31428886+oliviapy960825@users.noreply.github.com>
2022-06-12 20:29:13 -0700

    06/12 LC questions

---
+++ Leetcode_JAVA/LC120.java
@@ -0,0 +1,54 @@
+/*
+120. Triangle
+Medium
+
+Given a triangle array, return the minimum path sum from top to bottom.
+
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+ 
+
+Example 1:
+
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+
+Example 2:
+
+Input: triangle = [[-10]]
+Output: -10
+
+ 
+
+Constraints:
+
+    1 <= triangle.length <= 200
+    triangle[0].length == 1
+    triangle[i].length == triangle[i - 1].length + 1
+    -104 <= triangle[i][j] <= 104
+
+ 
+Follow up: Could you do this using only O(n) extra space, where n is the total number of rows in the triangle?
+*/
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        List<Integer> base = triangle.get(triangle.size() - 1);
+        for(int row = triangle.size() - 2; row >= 0; row--){
+            List<Integer> curr = new ArrayList<>();
+            for(int col = 0; col <= row; col++){
+                int best = Math.min(base.get(col), base.get(col + 1));
+                curr.add(best + triangle.get(row).get(col));
+            }
+            base = curr;
+        }
+        
+        return base.get(0);
+    }
+}
